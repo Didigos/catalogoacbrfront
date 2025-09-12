c9a28abd5869e902420de4aa6b8cdde9b0f8eb52
Didigos <diego.didigos@gmail.com>
2025-09-12 18:03:09 -0300

    feat: refactor App component and styles, replacing CSS with module styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import './App.css'
+import styles from "./App.module.css"
 
 function App() {
 
   return (
     <>
-    <h1>Página inicial</h1>
+    <header className={styles.header}>
+      <div className={styles.header__items}>
+        <div className={`${styles.header__nav__item}`}></div>
+        <div className={`${styles.header__title__item}`}></div>
+        <div className={`${styles.header__logo__item}`}></div>
+      </div>
+      <div className={styles.searchbar}></div>
+    </header>
     </>
   )
 }
