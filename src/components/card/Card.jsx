dd2f9e44acbc21ab77bf50a509088512d0869280
Diego Pires <diego.didigos@gmail.com>
2025-09-16 11:09:52 -0300

    feat: ajusta estrutura e estilos do componente Card para melhor exibição de especificações e forma de pagamento

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -17,79 +17,96 @@ const Card = () => {
         <div className={styles.product__image}>
           <img src="https://static.toiimg.com/thumb/resizemode-4,msid-117091955,imgsize-500,width-800/117091955.jpg" />
         </div>
-          <div className={styles.product__datas}>
-            <div className={styles.product__title}>
-              <h1>POCO X7 PRO</h1>
-            </div>
-            <div className={styles.product__specs}>
-              <div className={styles.specification}>
-                <div className={styles.specification__icon}>
-                  <FontAwesomeIcon icon={faMicrochip} size="2x" color="#FFB042" />
-                </div>
-                <div className={styles.specification__text}>
-                  <div className={styles.specification__text__title}>
-                    Processador
-                  </div>
-                  <div className={styles.specification__text__desc}>
-                    Snapdragon
-                  </div>
-                </div>
+        <div className={styles.product__datas}>
+          <div className={styles.product__title}>
+            <h1>POCO X7 PRO</h1>
+          </div>
+          <div className={styles.product__specs}>
+            <div className={styles.specification}>
+              <div className={styles.specification__icon}>
+                <FontAwesomeIcon icon={faMicrochip} size="2x" color="#FFB042" />
               </div>
-              <div className={styles.specification}>
-                <div className={styles.specification__icon}>
-                  <FontAwesomeIcon icon={faCamera} size="2x" color="#FFB042" />
+              <div className={styles.specification__text}>
+                <div className={styles.specification__text__title}>
+                  Processador
                 </div>
-                <div className={styles.specification__text}>
-                  <div className={styles.specification__text__title}>Camera</div>
-                  <div className={styles.specification__text__desc}>50Mp</div>
-                </div>
-              </div>
-              <div className={styles.specification}>
-                <div className={styles.specification__icon}>
-                  <FontAwesomeIcon
-                    icon={faBatteryFull}
-                    size="2x"
-                    color="#FFB042"
-                  />
-                </div>
-                <div className={styles.specification__text}>
-                  <div className={styles.specification__text__title}>Bateria</div>
-                  <div className={styles.specification__text__desc}>5110 mAh</div>
-                </div>
-              </div>
-              <div className={styles.specification}>
-                <div className={styles.specification__icon}>
-                  <FontAwesomeIcon icon={faDatabase} size="2x" color="#FFB042" />
-                </div>
-                <div className={styles.specification__text}>
-                  <div className={styles.specification__text__title}>
-                    Armazenamento
-                  </div>
-                  <div className={styles.specification__text__desc}>512 Gb</div>
-                </div>
-              </div>
-              <div className={styles.specification}>
-                <div className={styles.specification__icon}>
-                  <FontAwesomeIcon icon={faMemory} size="2x" color="#FFB042" />
-                </div>
-                <div className={styles.specification__text}>
-                  <div className={styles.specification__text__title}>Memória</div>
-                  <div className={styles.specification__text__desc}>12 Gb</div>
+                <div className={styles.specification__text__desc}>
+                  Snapdragon
                 </div>
               </div>
             </div>
-              <div className={styles.product__payment}>
-                <div className={styles.product__payment__title__container}>
-                  <span className={styles.product__payment__title}>
-                    Forma de Pagamento
-                  </span>
-                  <SelectPayment />
-                </div>
+            <div className={styles.specification}>
+              <div className={styles.specification__icon}>
+                <FontAwesomeIcon icon={faCamera} size="2x" color="#FFB042" />
+              </div>
+              <div className={styles.specification__text}>
+                <div className={styles.specification__text__title}>Camera</div>
+                <div className={styles.specification__text__desc}>50Mp</div>
               </div>
             </div>
+            <div className={styles.specification}>
+              <div className={styles.specification__icon}>
+                <FontAwesomeIcon
+                  icon={faBatteryFull}
+                  size="2x"
+                  color="#FFB042"
+                />
+              </div>
+              <div className={styles.specification__text}>
+                <div className={styles.specification__text__title}>Bateria</div>
+                <div className={styles.specification__text__desc}>5110 mAh</div>
+              </div>
+            </div>
+            <div className={styles.specification}>
+              <div className={styles.specification__icon}>
+                <FontAwesomeIcon icon={faDatabase} size="2x" color="#FFB042" />
+              </div>
+              <div className={styles.specification__text}>
+                <div className={styles.specification__text__title}>
+                  Armazenamento
+                </div>
+                <div className={styles.specification__text__desc}>512 Gb</div>
+              </div>
+            </div>
+            <div className={styles.specification}>
+              <div className={styles.specification__icon}>
+                <FontAwesomeIcon icon={faMemory} size="2x" color="#FFB042" />
+              </div>
+              <div className={styles.specification__text}>
+                <div className={styles.specification__text__title}>Memória</div>
+                <div className={styles.specification__text__desc}>12 Gb</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.product__payment}>
+            <div className={styles.product__payment__title__container}>
+              <span className={styles.product__payment__title}>
+                Forma de Pagamento
+              </span>
+            </div>
+              <SelectPayment />
+          </div>
+        </div>
       </section>
       <div className={styles.price__container}>
-        <p className={styles.price__text} style={{fontSize: ".9rem",fontWeight: "bold", color: "#666"}}>R$ 2.499,90 em até 10x de R$ 287,36 ou <br /> <span style={{fontSize: "1.4rem", color: "#46C262", fontWeight: "bold"}}>R$ 2.199,90 </span> <span style={{fontSize: ".7rem",color: "#46C262", fontWeight: "bold"}}> <br />No Pix / dinheiro</span></p>
+        <p
+          className={styles.price__text}
+          style={{ fontSize: ".9rem", fontWeight: "bold", color: "#666" }}
+        >
+          R$ 2.499,90 em até 10x de R$ 287,36 ou <br />{" "}
+          <span
+            style={{ fontSize: "1.4rem", color: "#46C262", fontWeight: "bold" }}
+          >
+            R$ 2.199,90{" "}
+          </span>{" "}
+          <span
+            style={{ fontSize: ".7rem", color: "#46C262", fontWeight: "bold" }}
+          >
+            {" "}
+            <br />
+            No Pix / dinheiro
+          </span>
+        </p>
       </div>
     </main>
   );
