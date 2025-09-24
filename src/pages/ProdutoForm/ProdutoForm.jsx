087b0777e0e5e577937f70e4035b85bdce3c8050
Diego Pires <diego.didigos@gmail.com>
2025-09-24 09:52:50 -0300

    feat: implementa formulário de edição de produto com validação e carregamento de dados

--- src/pages/ProdutoForm/ProdutoForm.jsx
+++ src/pages/ProdutoForm/ProdutoForm.jsx
@@ -1,17 +1,63 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./ProdutoForm.module.css"
 import { useParams } from "react-router"
+import { get, useForm } from "react-hook-form";
+import axios from "axios";
+
+
+
 const ProdutoForm = ()=>{
 
     const { id } = useParams();
+    const [getProdutos, setGetProdutos] = useState([]);
+    const { register, handleSubmit, formState: { errors }, } = useForm({
+        defaultValues: {
+            nome: getProdutos.nome || "default",
+            marca: getProdutos.marca || "default",
+            preco: getProdutos.precobase || "default",
+
+        }
+    });
+
+    const onSubmit = (data)=>{
+        console.log('dados do formuladior:', data )
+    }
 
     useEffect(() => {
-       console.log('id do produto: ', id)
+       const getProduto = async () => {
+        const response = await axios.get(`http://localhost:3000/smartphones/${id}`);
+        setGetProdutos(response.data);
+       }
+       getProduto();
     }, [id]);
 
     return (
-        <main>
-            <h1 className={styles.main__title}>Produto Form</h1>
+        <main className={styles.main}>
+            <h2>Editar produto</h2>
+            <form className={styles.edit__form} onSubmit={handleSubmit(onSubmit)}>
+                <label className={styles.edit__name__label} htmlFor="nome">Nome</label>
+                <input
+                    name="nome"
+                    type="text"
+                    placeholder={getProdutos.nome || "none"}
+                    {...register("nome", { required: true })}
+                />
+                <label className={styles.edit__marca__label} htmlFor="marca">Marca</label>
+                <input
+                    name="marca"
+                    type="text"
+                    placeholder={getProdutos.marca || "none"}
+                    {...register("marca", { required: true })}
+                />
+                <label className={styles.edit__preco__label} htmlFor="preco">Preço</label>
+                <input
+                    name="preco"
+                    type="text"
+                    placeholder={getProdutos.precobase || "none"}
+                    {...register("preco", { required: true })}
+                />
+                <button type="submit">Salvar</button>
+            </form>
         </main>
     )
 }
--- src/pages/Produtos/Produtos.jsx
+++ src/pages/Produtos/Produtos.jsx
@@ -1,36 +1,53 @@
-import styles from "./Produtos.module.css"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faPenToSquare } from "@fortawesome/free-solid-svg-icons"
-import { useContext} from "react"
-import ProdutosContext from "../../context/produtosContext"
+import styles from "./Produtos.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { useContext } from "react";
+import ProdutosContext from "../../context/produtosContext";
 import { useNavigate } from "react-router";
-const Produtos = ()=>{
-    const { produtos, loading } = useContext(ProdutosContext)
-    const navigate = useNavigate();
+const Produtos = () => {
+  const { produtos, loading } = useContext(ProdutosContext);
+  const navigate = useNavigate();
 
-    return(
-        <main className={styles.produtos__main}>
-            <header className={styles.produtos__header}>
-                <div className={styles.produtos__header__icon}>
-                    <FontAwesomeIcon icon={faPenToSquare} color="#ffffff" size="3x"/>
-                </div>
-                <h1 className={styles.produtos__header__title}>Edição de Produtos</h1>
-            </header>
-             <section className={styles.produtos__section}>
-                {!loading && produtos.length > 0 ? produtos.map((produto)=>(
-                <div key={produto.id} className={styles.produtos__item}>
-                    <h1 className={styles.produtos__item__title}>{produto.nome}</h1>
-                    <button onClick={() => navigate(`/produtos/${produto.id}`)} className={styles.produtos__item__text}>Clique Aqui para Editar</button>
-               </div>
-                )) : <section className={styles.no__item}>
-                <h2 className={styles.no__item__title}>Nenhum produto encontrado</h2>
-                <span className={styles.no__item__text}>Adicione produtos para que eles apareçam aqui</span>
-               </section>}
+  return (
+    <main className={styles.produtos__main}>
+      <header className={styles.produtos__header}>
+        <div className={styles.produtos__header__icon}>
+          <FontAwesomeIcon icon={faPenToSquare} color="#ffffff" size="3x" />
+        </div>
+        <h1 className={styles.produtos__header__title}>Edição de Produtos</h1>
+      </header>
+      <div className={styles.produtos__warning}>
+        <FontAwesomeIcon className={styles.produtos__warning__icon} icon={faCircleExclamation} size="1x" color="#fff" />{" "}
+        <span className={styles.produtos__warning__text}>
+          Uma vez que o produto for deletado não há como recuperar.
+        </span>
+      </div>
+      <section className={styles.produtos__section}>
+        {!loading && produtos.length > 0 ? (
+          produtos.map((produto) => (
+            <div key={produto.id} className={styles.produtos__item}>
+              <h1 className={styles.produtos__item__title}>{produto.nome}</h1>
+              <button
+                onClick={() => navigate(`/produtos/${produto.id}`)}
+                className={styles.produtos__item__text}
+              >
+                Clique Aqui para Editar
+              </button>
+            </div>
+          ))
+        ) : (
+          <section className={styles.no__item}>
+            <h2 className={styles.no__item__title}>
+              Nenhum produto encontrado
+            </h2>
+            <span className={styles.no__item__text}>
+              Adicione produtos para que eles apareçam aqui
+            </span>
+          </section>
+        )}
+      </section>
+    </main>
+  );
+};
 
-            </section>
-
-        </main>
-    )
-}
-
-export default Produtos 
+export default Produtos;
