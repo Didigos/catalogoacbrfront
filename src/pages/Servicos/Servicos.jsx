ab4f2a4d1bbf4035bc334a7c62b35ad370bde4d4
Diego Pires <diego.didigos@gmail.com>
2025-11-11 17:58:51 -0300

    Adicionar funcionalidade de formulário e ajustes de estilo na seção de Serviços: implementar seleção de situação e atualizar estilos de elementos

--- src/pages/Servicos/Servicos.jsx
+++ src/pages/Servicos/Servicos.jsx
@@ -1,5 +1,7 @@
 import styles from "./Servicos.module.css";
+import { useForm } from "react-hook-form";
 const Servicos = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
   return (
     <main className={styles.servicos__main}>
       <header className={styles.servicos__header}>
@@ -13,13 +15,9 @@ const Servicos = () => {
       <ul className={styles.list__section}>
         <li className={styles.list__item}>
           <div className={styles.list__item__title}>
-            <span>OS Nº 001</span>
+            <span>Diego Pires Gomes</span>
           </div>
             <div className={styles.list__item__container}>
-                <div className={styles.container__clientName}>
-                    <h1>CLIENTE</h1>
-                    <span>Diego Pires Gomes</span>
-                </div>
                 <div className={styles.date__join}>
                     <h1>ENTRADA</h1>
                     <span>11/10/2025</span>
@@ -32,6 +30,16 @@ const Servicos = () => {
                     <div className={`${styles.action__edit} ${styles.action__btn}`}></div>
                     <div className={`${styles.action__delete} ${styles.action__btn}`}></div>
                     <div className={`${styles.action__print} ${styles.action__btn}`}></div>
+                </div>
+                <div className={styles.item__situations}>
+                    <select 
+                    className={styles.item__select}
+                    {...register("situacao")}
+                    >
+                        <option value="pendente">Pendente</option>
+                        <option value="em_andamento">Em Andamento</option>
+                        <option value="concluido">Concluído</option>
+                    </select>
                 </div>
             </div>
         </li>
