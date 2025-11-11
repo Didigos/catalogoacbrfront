c9e429055c46ac2f13cbde0fb8da953423eda928
Diego Pires <diego.didigos@gmail.com>
2025-11-11 16:08:00 -0300

    Ajustar estilos da seção de Serviços: corrigir a classe .seach__container e adicionar estilos para .search__input

--- src/pages/Servicos/Servicos.jsx
+++ src/pages/Servicos/Servicos.jsx
@@ -1,21 +1,24 @@
-import styles from './Servicos.module.css'
-const Servicos = ()=>{
-    return (
-        <main className={styles.servicos__main}>
-            <header className={styles.servicos__header}>
-                <h1 className={styles.header__titulo}>Lista de OS</h1>
-            </header>
+import styles from "./Servicos.module.css";
+const Servicos = () => {
+  return (
+    <main className={styles.servicos__main}>
+      <header className={styles.servicos__header}>
+        <h1 className={styles.header__titulo}>Lista de OS</h1>
+      </header>
 
-            <ul className={styles.list__section}>
-                <li className={styles.list__item}>
-                    <div className={styles.list__item__title}>
-                        <span>OS N: 001</span>
-                    </div>
-                </li>
-            </ul>
+      <div className={styles.seach__container}>
+        <input placeholder="digite o nome do cliente" className={styles.search__input} type="search" name="" id="" />
+      </div>
 
-        </main>
-    )
-}
+      <ul className={styles.list__section}>
+        <li className={styles.list__item}>
+          <div className={styles.list__item__title}>
+            <span>OS N: 001</span>
+          </div>
+        </li>
+      </ul>
+    </main>
+  );
+};
 
-export default Servicos
+export default Servicos;
